642a5f4043ba14eeb251c38c4c05ec33fe7b2133
Damo <wyyxdgm@163.com>
2023-01-02 17:38:46 +0800

    test: webgl capture

--- index/index.js
+++ index/index.js
@@ -27,20 +27,33 @@ const triangleVertices = [
 ];
 
 Page({
+  data: {},
   onLoad() {
     const query = wx.createSelectorQuery()
     query.select('#myCanvas').node().exec((res) => {
       const canvas = res[0].node
+      this.canvas = canvas;
       this._render(canvas)
+      const gl = canvas.getContext('webgl')
+      console.log(gl)
+    })
+    query.select('#captureCanvas').node().exec((res) => {
+      console.log(res);
+      const captureCanvas = res[1].node
+      this.data.captureCanvas = captureCanvas;
+      this.data.captureCanvasContext = captureCanvas.getContext('2d');
+      console.log(this.data.captureCanvas, this.data.captureCanvasContext,)
     })
   },
 
   _render(canvas) {
     const gl = canvas.getContext('webgl')
+    console.log(gl)
     if (!gl) {
       console.error('gl init failed', gl)
       return
     }
+    this.gl = gl;
     gl.viewport(0, 0, 300, 300)
     const vertShader = gl.createShader(gl.VERTEX_SHADER)
     gl.shaderSource(vertShader, vs)
@@ -89,5 +102,52 @@ Page({
     }
 
     canvas.requestAnimationFrame(draw)
+  },
+
+  takePhoto(e) {
+
+    return new Promise((resolve, reject) => {
+      //webgl截屏直接重用当前canvas，因为尺寸肯定比最终要画的webgl大，所以就算iOS端可能canvas的渲染分辨率没有原生那么大，也差不多够大了。
+      //poster组件会先把canvas放到屏幕外（left属性），等到load结束再拿回来。
+
+      let gl = this.gl;
+      let [w, h] = [gl.drawingBufferWidth, gl.drawingBufferHeight];
+
+      // 3d截图
+      let readPixelBuffer = new Uint8Array(w * h * 4);
+      let buffer2 = new Uint8Array(w * h * 4);
+      gl.readPixels(0, 0, w, h, gl.RGBA, gl.UNSIGNED_BYTE, readPixelBuffer);
+
+      const captureCanvas = this.data.captureCanvas;
+      debugger
+      const context = this.data.captureCanvasContext;
+      let imageData = context.createImageData(w, h);
+      // 画到canvas上
+      captureCanvas.width = w;
+      captureCanvas.height = h;
+      imageData.data.set(new Uint8ClampedArray(readPixelBuffer));
+      context.clearRect(0, 0, w, h);
+      context.putImageData(imageData, 0, 0);
+
+      //保存到相册
+      // wx.canvasToTempFilePath({
+      //   canvas: captureCanvas,
+      //   fileType: "png",
+      //   x: 0,
+      //   y: 0,
+      //   width: w,
+      //   height: h,
+      //   destWidth: w,
+      //   destHeight: h,
+
+      //   success: result => {
+      //     this.image = captureCanvas.createImage();
+      //     this.image.src = result.tempFilePath;
+      //     this.image.onload = () => resolve();
+      //     this.image.onerror = reject;
+      //   },
+      //   fail: reject,
+      // });
+    });
   }
 })
